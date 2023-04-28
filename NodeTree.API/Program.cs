a6aee86b6db875fc6f2d14ee702ecadffd3171a3
darek.filar <darek.filar@gmail.com>
2023-04-28 07:31:27 +0200

    Added node service

--- NodeTree.API/Program.cs
+++ NodeTree.API/Program.cs
@@ -1,6 +1,8 @@
 using NodeTree.DB;
 using NodeTree.DB.Repositories.Abstract;
 using NodeTree.DB.Repositories.Concrete;
+using NodeTree.INFRASTRUCTURE.Services.Abstract;
+using NodeTree.INFRASTRUCTURE.Services.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<NodeTreeDbContext>();
 builder.Services.AddScoped<INodeRepository, NodeRepository>();
-
+builder.Services.AddScoped<INodeService, NodeService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
