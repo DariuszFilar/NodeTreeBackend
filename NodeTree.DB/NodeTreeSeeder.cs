e95fda11810bf55de849a9b511d3ff6629e784e4
darek.filar <darek.filar@gmail.com>
2023-04-28 09:50:03 +0200

    Added seeder

---
+++ NodeTree.DB/NodeTreeSeeder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NodeTree.DB.Entities;
+
+namespace NodeTree.DB
+{
+    public class NodeTreeSeeder
+    {
+        private readonly NodeTreeDbContext _context;
+        public NodeTreeSeeder(NodeTreeDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Database.CanConnect())
+            {
+                var pendingMigrations = _context.Database.GetPendingMigrations();
+                if (pendingMigrations != null && pendingMigrations.Any())
+                {
+                    _context.Database.Migrate();
+                }
+
+                if (!_context.Nodes.Any())
+                {
+                    Node masterNode = new()
+                    {
+                        Id = 0,
+                        Name = "root"
+                    };
+
+                    _context.Nodes.Add(masterNode);
+                    _context.SaveChanges();
+                }
+            }
+        }
+    }
+}
