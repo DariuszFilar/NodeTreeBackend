41cfe39e11cfff2b91acb8cd5919a793797b340f
darek.filar <darek.filar@gmail.com>
2023-04-28 07:09:12 +0200

    Added node controller

---
+++ NodeTree.API/Controllers/NodeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NodeTree.API.Controllers
+{
+    public class NodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
