5a938b3eaf707ffd76d12a9ec6b1029179e635c6
darek.filar <darek.filar@gmail.com>
2023-04-28 11:01:44 +0200

    Changed add node method

--- NodeTree.INFRASTRUCTURE/Repositories/Concrete/NodeRepository.cs
+++ NodeTree.INFRASTRUCTURE/Repositories/Concrete/NodeRepository.cs
@@ -19,5 +19,31 @@ namespace NodeTree.INFRASTRUCTURE.Repositories.Concrete
                 .Where(n => n.ParentId == parentId)
                 .ToListAsync();
         }
+
+        public async Task<Node> GetTreeByTreeNameAsync(string treeName)
+        {
+            Node parent = await _context.Nodes
+                .Include(n => n.Children)
+                .Where(n => n.TreeName == treeName && n.ParentId == null)
+                .FirstOrDefaultAsync();
+
+            if (parent != null)
+            {
+                LoadChildrenRecursively(parent);
+            }
+
+            return parent;
+        }
+        private void LoadChildrenRecursively(Node node)
+        {
+            if (node == null) return;
+
+            foreach (var child in node.Children)
+            {
+                _context.Entry(child).Reference(n => n.Parent).Load();
+                _context.Entry(child).Collection(n => n.Children).Load();
+                LoadChildrenRecursively(child);
+            }
+        }
     }
 }
--- NodeTree.INFRASTRUCTURE/Requests/CreateNodeRequest.cs
+++ NodeTree.INFRASTRUCTURE/Requests/CreateNodeRequest.cs
@@ -2,7 +2,8 @@
 {
     public class CreateNodeRequest
     {
-        public long? ParentId { get; set; }
-        public string Name { get; set; }
+        public long ParentId { get; set; }
+        public string NodeName { get; set; }
+        public string TreeName { get; set; }
     }
 }
